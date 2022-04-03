2f392e42a0c4ff33d997236c11836f0656923df7
MarioLuiz <marioluiz.arruda@gmail.com>
2022-04-03 16:47:18 -0400

    Java Loops I

---
+++ br.com.hacker-hank.prepere.java/Introduction/javaLoopsI.java
@@ -0,0 +1,20 @@
+package Introduction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class javaLoopsI {
+	//https://www.hackerrank.com/challenges/java-loops-i/problem
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        for (int i = 1; i <= 10; i++) {
+			System.out.println(N + " x " + i + " = " + N*i);
+		}
+        
+        bufferedReader.close();
+    }
+
+}
