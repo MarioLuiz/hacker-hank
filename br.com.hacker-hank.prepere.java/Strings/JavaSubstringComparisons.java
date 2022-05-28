fdcbefb19c8685ae0f52cf54e6819612447a3c19
MarioLuiz <marioluiz.arruda@gmail.com>
2022-05-27 23:00:22 -0400

    Java Substring Comparisons

--- br.com.hacker-hank.prepere.java/Strings/JavaSubstringComparisons.java
+++ br.com.hacker-hank.prepere.java/Strings/JavaSubstringComparisons.java
@@ -1,2 +1,37 @@
-package Strings;public class JavaSubstringComparisons {
+package Strings;
+
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class JavaSubstringComparisons {
+    // https://www.hackerrank.com/challenges/java-string-compare/problem
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String palavra = scan.next();
+        int tamanho = scan.nextInt();
+        int quantidadePalavras = palavra.length() - tamanho;
+        SortedSet<String> sets = new TreeSet<String>();
+        for (int i = 0; i <= quantidadePalavras; i++) {
+            sets.add(palavra.substring(i, i + tamanho));
+        }
+        System.out.println(sets.first());
+        System.out.println(sets.last());
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        int quantidadePalavras = s.length() - k;
+        SortedSet<String> sets = new TreeSet<String>();
+        for (int i = 0; i <= quantidadePalavras; i++) {
+            sets.add(s.substring(i, i + k));
+        }
+        smallest = sets.first();
+        largest = sets.last();
+
+        return smallest + "\n" + largest;
+    }
 }
